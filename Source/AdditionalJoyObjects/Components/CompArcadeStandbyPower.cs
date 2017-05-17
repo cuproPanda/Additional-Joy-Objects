1db62c13bef6ad0c3928e8f51a1d0e83874fcb51
cuproPanda <cupropanda@gmail.com>
2017-05-17 15:01:01 -0400

    Renamed mod folder, A17u update

---
+++ Source/AdditionalJoyObjects/Components/CompArcadeStandbyPower.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using RimWorld;
+using Verse;
+
+namespace AdditionalJoyObjects {
+
+  internal class CompArcadeStandbyPower : ThingComp {
+
+    private List<Thing> pawnsInCell;
+    private CompPowerTrader powerComp;
+    private CompFlickable flickableComp;
+    private float idlePower = 5f;
+    private float fullPower;
+
+
+    public override void PostSpawnSetup(bool respawningAfterLoad) {
+      base.PostSpawnSetup(respawningAfterLoad);
+
+      powerComp = parent.GetComp<CompPowerTrader>();
+      flickableComp = parent.GetComp<CompFlickable>();
+      fullPower = powerComp.Props.basePowerConsumption;
+    }
+
+
+    public override void CompTick() {
+      base.CompTick();
+
+      // Determine the power usage based on if the arcade is being used
+      if (parent.Spawned) {
+        powerComp.PowerOutput = InUse() ? fullPower : idlePower; 
+      }
+    }
+
+
+    private bool InUse() {
+      pawnsInCell = parent.InteractionCell.GetThingList(parent.Map).Where(p => p is Pawn).ToList();
+
+      // If a pawn is using this arcade, comsume full power
+      // Otherwise, only use standby power
+      for (int p = 0; p < pawnsInCell.Count; p++) {
+        if (pawnsInCell[p] is Pawn) {
+          Pawn pawn = pawnsInCell[p] as Pawn;
+          if (pawn.CurJob != null && (pawn.CurJob.targetA != null && pawn.CurJob.targetA.Thing == parent) || (pawn.CurJob.targetB != null && pawn.CurJob.targetB.Thing == parent)) {
+            if (flickableComp.SwitchIsOn && !parent.IsBrokenDown()) {
+              return true;
+            }
+          }
+        }
+      }
+      return false;
+    }
+  }
+}
