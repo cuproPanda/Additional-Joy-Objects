04d9a10f9508854e4ff4beced61448eac4b6d0dd
cuproPanda <cupropanda@gmail.com>
2017-06-12 18:52:11 -0400

    Fixed chandelier ticking

--- Source/AdditionalJoyObjects/Components/CompHanger.cs
+++ Source/AdditionalJoyObjects/Components/CompHanger.cs
@@ -8,18 +8,9 @@ namespace AdditionalJoyObjects {
   /// </summary>
   public class CompHanger : ThingComp {
 
-    private Map parentMap;
-
     /// <summary></summary>
     public CompProperties_Hanger Props {
       get { return (CompProperties_Hanger)props; }
-    }
-
-
-    public override void PostSpawnSetup(bool respawningAfterLoad) {
-      base.PostSpawnSetup(respawningAfterLoad);
-
-      parentMap = parent.Map;
     }
 
 
@@ -35,8 +26,10 @@ namespace AdditionalJoyObjects {
         // Get the tile behind this object
         IntVec3 c = parent.Position - parent.Rotation.FacingCell;
         // Minify this if the wall is missing
-        Building edifice = c.GetEdifice(parentMap);
-        if (edifice == null || (edifice.def != ThingDefOf.Wall && !edifice.def.building.isNaturalRock && (edifice.Faction != Faction.OfPlayer || (LinkFlags.Wall & edifice.def.graphicData.linkFlags) == LinkFlags.None))) {
+        Building edifice = c.GetEdifice(parent.Map);
+        if (edifice == null || edifice.def == null || (edifice.def != ThingDefOf.Wall &&
+          ((edifice.Faction == null || edifice.Faction != Faction.OfPlayer) ||
+          edifice.def.graphicData == null || edifice.def.graphicData.linkFlags == 0 || (LinkFlags.Wall & edifice.def.graphicData.linkFlags) == LinkFlags.None))) {
           Minify();
         }
       }
@@ -44,17 +37,20 @@ namespace AdditionalJoyObjects {
       if (Props.hangingType == HangingType.Ceiling) {
         // Minify this if the ceiling is missing
         int occCells = 0;
-        int roofCells = 0;
+        int unroofedCells = 0;
         foreach (IntVec3 current in parent.OccupiedRect()) {
           occCells++;
-          if (!parentMap.roofGrid.Roofed(current)) {
-            roofCells++;
+          if (!parent.Map.roofGrid.Roofed(current)) {
+            unroofedCells++;
+          }
+          if (current.GetEdifice(parent.Map) == null || current.GetEdifice(parent.Map).def == null) {
+            continue;
           }
           if (current.GetEdifice(parent.Map).def.blockWind == true || current.GetEdifice(parent.Map).def.holdsRoof == true) {
             Minify();
           }
         }
-        if (((float)(occCells - roofCells) / occCells) < 0.5f) {
+        if (((float)(occCells - unroofedCells) / occCells) < 0.5f) {
           Minify();
         }
       }
@@ -64,8 +60,8 @@ namespace AdditionalJoyObjects {
     /// <summary> Minifies the package</summary>
     public virtual void Minify() {
       MinifiedThing package = parent.MakeMinified();
-      GenPlace.TryPlaceThing(package, parent.Position, parentMap, ThingPlaceMode.Near);
-      SoundDef.Named("ThingUninstalled").PlayOneShot(new TargetInfo(parent.Position, parentMap));
+      GenPlace.TryPlaceThing(package, parent.Position, parent.Map, ThingPlaceMode.Near);
+      SoundDef.Named("ThingUninstalled").PlayOneShot(new TargetInfo(parent.Position, parent.Map));
     }
   }
 }
